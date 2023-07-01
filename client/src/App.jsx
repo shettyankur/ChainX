6a48b2dad681d627439e7b6696c7697ae9f48255
New User <anjaliss.2022@gmail.com>
2023-07-01 21:22:24 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { Navbar, Market } from "./components";
+
+const App = () => (
+  <div className="min-h-screen">
+    <div className="gradient-bg-welcome">
+      <Navbar />
+      <Market />
+    </div>
+  </div>
+);
+
+export default App;
